7767fc3ba6fb0f1288eafc948be77287ea47fed5
Julia Romanowska <romanowska.j@gmail.com>
2019-12-09 10:54:26 +0100

    adding a new function: attaching an extra covariate variable to a 'haplin.data' or 'haplin.ready' object, useful afterwards when implementing the GxMe and PoOxMe functionality

---
+++ R/addCovar.R
@@ -0,0 +1,54 @@
+#' Adding a covariate variable
+#'
+#' Easily add a column to \code{cov.data} part of the "haplin.data" or "haplin.ready" object
+#'
+#' Add a character or integer vector to the "cov.data" part of the "haplin.data" or
+#'   "haplin.ready" object. This is useful e.g., when preparing for stratification
+#'   analysis (with \code{\link{haplinStrat}}). Note that all the parameters are required
+#'   and that \code{covar} needs to be a vector of the length equal to the number of
+#'   rows in the \code{data.in}.
+#'
+#' @param data.in The input data, "haplin.data" or "haplin.ready" object.
+#' @param covar The vector to be added.
+#' @param c.name The name of the column that will be added to \code{cov.data}.
+#'
+#' @return An object of the same class as \code{data.in} but with additional covariates.
+#'
+#' @export
+#'
+addCovar <- function(
+	data.in = stop( "Data.in must be given!", call. = FALSE )
+	, covar = stop( "covar must be given!", call. = FALSE )
+	, c.name = stop( "c.name must be given!", call. = FALSE )
+){
+	# check the input params
+	if( !( is( data.in, "haplin.data" ) || is( data.in, "haplin.ready" ) ) ){
+		stop( "The input data is not in the correct format!", call. = FALSE )
+	}
+
+	if( !is.vector( covar ) ){
+		stop( "'covar' is not a vector!", call. = FALSE )
+	}
+	if( length( covar ) != nrow( data.in$gen.data[[ 1 ]] ) ){
+		stop( paste0( "'covar' length (", length( covar ), ") is not equal to
+					  the number of rows in the data (", nrow( data.in$gen.data[[ 1 ]] ),
+					  ")!"), call. = FALSE )
+	}
+
+	if( length( c.name ) != 1 ){
+		stop( "'c.name' should be just one string!", call. = FALSE )
+	}
+
+	# adding the data
+	new.data <- data.in
+	if( is.null( data.in$cov.data ) ){
+		# need to create new cov.data
+		new.data$cov.data <- matrix( data = covar, ncol = 1 )
+		colnames( new.data$cov.data ) <- c.name
+	} else {
+		new.data$cov.data <- cbind( data.in$cov.data, covar )
+		colnames( new.data$cov.data ) <- c( colnames( data.in$cov.data ), c.name )
+	}
+
+	return( new.data )
+}
---
+++ tests/testthat/test_3_addCovar.R
@@ -0,0 +1,62 @@
+context( "Testing addCovar function." )
+
+source( "common_vars.R" )
+
+dir.exmpl <- system.file( "extdata", package = "Haplin" )
+exemplary.file1 <- paste0( dir.exmpl, "/HAPLIN.trialdata.txt" )
+
+my.gen.data.haplin <- Haplin::genDataRead( file.in = exemplary.file1, file.out = "trial_data1",
+	dir.out = ".", format = "haplin", n.vars = 0, overwrite = TRUE )
+
+exemplary.file2 <- paste0( dir.exmpl, "/HAPLIN.trialdata2.txt" )
+my.gen.data.haplin2 <- Haplin::genDataRead( file.in = exemplary.file2, file.out = "trial_data2",
+	dir.out = ".", format = "haplin", n.vars = 2, allele.sep = "", overwrite = TRUE )
+
+exemplary.file3 <- paste0( dir.exmpl, "/exmpl_data.ped" )
+my.gen.data <- Haplin::genDataRead( exemplary.file3, file.out = "ped_data", dir.out = ".",
+	format = "ped", overwrite = TRUE )
+
+test_that( "Adding covar to haplin-formatted data without covariates", {
+	covar.vect <- sample( x = 1:3, size = nrow( my.gen.data.haplin$gen.data[[ 1 ]]),
+						  replace = TRUE )
+	c.name <- "add.c"
+	new.data <- addCovar( my.gen.data.haplin, covar = covar.vect, c.name = c.name )
+	expect_s3_class( new.data, class( my.gen.data.haplin ) )
+	expect_true( is( new.data$cov.data, "matrix" ) )
+	expect_equal( ncol( new.data$cov.data ), 1 )
+	expect_identical( colnames( new.data$cov.data ), c.name )
+} )
+
+test_that( "Adding covar to haplin-formatted data with covariates", {
+	covar.vect <- sample( x = 1:3, size = nrow( my.gen.data.haplin2$gen.data[[ 1 ]]),
+						  replace = TRUE )
+	c.name <- "add.c"
+	new.data <- addCovar( my.gen.data.haplin2, covar = covar.vect, c.name = c.name )
+	expect_s3_class( new.data, class( my.gen.data.haplin2 ) )
+	expect_true( is( new.data$cov.data, "matrix" ) )
+	expect_equal( ncol( new.data$cov.data ),
+					  ncol( my.gen.data.haplin2$cov.data ) + 1 )
+	expect_identical( colnames( new.data$cov.data ),
+					  c( colnames( my.gen.data.haplin2$cov.data ), c.name ) )
+} )
+
+test_that( "Adding covar to ped-formatted data", {
+	covar.vect <- sample( x = 1:3, size = nrow( my.gen.data$gen.data[[ 1 ]]),
+						  replace = TRUE )
+	c.name <- "add.c"
+	new.data <- addCovar( my.gen.data, covar = covar.vect, c.name = c.name )
+	expect_s3_class( new.data, class( my.gen.data ) )
+	expect_true( is( new.data$cov.data, "matrix" ) )
+	expect_equal( ncol( new.data$cov.data ),
+					  ncol( my.gen.data$cov.data ) + 1 )
+	expect_identical( colnames( new.data$cov.data ),
+					  c( colnames( my.gen.data$cov.data ), c.name ) )
+} )
+
+test_that( "Wrong length of covar vector", {
+	covar.vect <- sample( x = 1:3, size = nrow( my.gen.data.haplin$gen.data[[ 1 ]]) - 3,
+						  replace = TRUE )
+	c.name <- "add.c"
+	expect_error( addCovar( my.gen.data.haplin, covar = covar.vect, c.name = c.name ) )
+} )
+
