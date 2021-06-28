85455e02192689c11bdcaf4c8dcadad36388b9c3
Julia Romanowska <Julia.Romanowska@uib.no>
2021-06-28 21:30:36 +0200

    creating mock genotype data

--- inst/make_examplary_data.R
+++ inst/make_examplary_data.R
@@ -1,3 +1,4 @@
+# --- create DNAm mock dataset ----
 # creating exemplary data: matrix with values [0;1]
 nrow.out <- 200
 ncol.out <- 400
@@ -38,3 +39,33 @@ write( row.names, file = only.row.names, ncolumns = length( row.names ) )
 
 # write only the column names
 write( col.names, file = only.col.names, ncolumns = length( col.names ) )
+
+# --- create genotype mock dataset ----
+library(Haplin)
+
+set.seed( 146 )
+
+hapSim(
+  nall = 2,
+  n.strata = 3,
+  cases = list(c(mfc = 66), c(mfc = 67), c(mfc = 67)),
+  # controls = c(mfc = 800),
+  haplo.freq = rep(0.25, 2),
+  RR = list(c(1.3,1), c(1,1), c(0.6,1)),
+  RRstar = c(1,1),
+  gen.missing.cases = NULL, gen.missing.controls = NULL,
+  n.sim = 1, dire = "strata3", ask = TRUE, verbose = TRUE, cpus = 1
+)
+
+data_in <- genDataRead(
+  file.in = "strata3/sim1.dat",
+  dir.out = "strata3",
+  format = "haplin",
+  n.vars = 1, allele.sep = " ", col.sep = " "
+)
+# data_in <- genDataLoad(
+#   filename = "sim1", dir.in = "~/Naukowe/Haplin_sim_tests/strata3/"
+# )
+data_in
+
+showPheno(data_in)
