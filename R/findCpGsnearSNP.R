d29f4c7aaa1b4ea680a2d845c939ac805d23afb1
ellisifnygaard <Ellisif.Nygaard@uib.no>
2025-03-28 19:33:40 +0100

    formatted arguments etc. using \code{}, fixed some typos, tweaked some of the wording, added an example to findCpGsnearSNP documentation, and ran devtools::document() so that the .Rd files (and some other files as well) got updated.

--- R/findCpGsnearSNP.R
+++ R/findCpGsnearSNP.R
@@ -1,43 +1,60 @@
 #' Finding CpGs near a SNP
 #'
-#' @description 
-#' This is a basic function for searching for CpGs in the vicinity of a given
-#' SNP.
+#' @description This is a basic function for searching for CpGs in the vicinity
+#'   of a given SNP.
 #'
-#' @details 
-#' Given the position and chromosome number of a particular SNP, along with the 
-#' positions of CpGs on the same chromosome, the function will return a 
-#' data.frame with nearby CpGs (within a specified number of base pairs).  
-#' 
-#' The positions of the CpGs should refer to the cytosine nucleotide of the CpG 
-#' (or CpH) dinucleotide.  
-#'   
-#' A CpG is classified as 'near' the SNP if the position of its cytosine 
-#' nucleotide falls within the following open interval:  
-#' `(snp$coord - range , snp$coord + range)`.
-#' 
-#' This function uses the 1-based coordinate system.
-#'  
+#' @details Given the position and chromosome number of a particular SNP, along
+#'   with the positions of CpGs on the same chromosome, the function will return
+#'   a data.frame with nearby CpGs (within a specified number of base pairs).
+#'
+#'   The positions of the CpGs should refer to the cytosine nucleotide of the
+#'   CpG (or CpH) dinucleotide.
+#'
+#'   A CpG is classified as 'near' the SNP if the position of its cytosine
+#'   nucleotide falls within the following open interval: `(snp$coord - range ,
+#'   snp$coord + range)`.
+#'
+#'   This function uses the 1-based coordinate system.
+#'
 #' **Please note** that you should ensure that every CpG in `cpgs` and the SNP
-#'  in `snp` are located on the same chromosome.
+#'   in `snp` are located on the same chromosome.
 #'
-#' @param snp A named list with the SNP's name ("marker"), chromosome no. ("chr")
-#'     and coordinate ("coord").
-#' @param cpgs A list or data.frame containing all CpGs on the chromosome where
-#'   the SNP is located. Must contain only two elements/columns: one named "id" 
-#'   (unique CpG locus cluster ID, cg#) and one named "coord" (coordinate of the
-#'   cytosine nucelotide of CpG locus).
+#' @param snp A named list (or one-row data.frame) containing the SNP's
+#'   information. The object must contain three elements (or columns):\cr
+#'   \code{"marker"} (a character string containing the SNP's name/ID),\cr
+#'   \code{"chr"} (an integer representing the chromosome number), and\cr
+#'   \code{"coord"} (an integer representing the SNP's position on \code{chr}).
+#' @param cpgs A named list (or a data.frame with one row per CpG) containing
+#'   the ID(s) and coordinate(s) of all the CpGs on the chromosome where the SNP
+#'   is located.\cr
+#'   Must contain only two elements/columns:\cr 
+#'   \code{"id"} (containing unique CpG locus cluster IDs; cg#), and\cr 
+#'   \code{"coord"} (containing integers representing the coordinates of the 
+#'   cytosine nucleotides of the CpG loci).
 #' @param range An integer specifying the desired maximum number of base pairs
-#'   between the given SNP and CpGs. The default is 5000. The search for 
-#'   `cpgs$coord` is restricted to the open interval 
-#'   (snp$coord - range, snp$coord + range).
+#'   between the given SNP and CpGs. The default is 5000. The search for
+#'   `cpgs$coord` is restricted to the open interval \code{(snp$coord - range,
+#'   snp$coord + range)}.
 #' @param verbose Whether to print information about the result when available
-#'   (default: FALSE).
+#'   (default: \code{FALSE}).
 #'
-#' @return A data.frame with CpGs with columns named: "id", "coord".
+#' @return A data.frame with columns named \code{id} and \code{coord},
+#'   containing one row per CpG that is 'nearby' the SNP. Returns \code{NULL} if
+#'   none of the CpGs in \code{cpgs} are within the given range of the SNP.
+#'
+#' @examples
+#' # Named list with SNP information:
+#' snp_list <- list(marker = "rs123", chr = 1, coord = 1000000)
+#'
+#' # Data frame with CpGs:
+#' cpgs_df <- data.frame( id = c("cg001", "cg002", "cg003"),
+#'                        coord = c(999000, 1000500, 1005000))
+#'
+#' # Find CpGs within 5000 bp of SNP
+#' findCpGsnearSNP(snp = snp_list, cpgs = cpgs_df)
 #'
 #' @export
-#'
+#' 
 findCpGsnearSNP <- function( snp = stop( "'snp' is required!", call. = FALSE ),
 							 cpgs = stop( "'cpgs' is required!", call. = FALSE ),
 							 range = 5000,
